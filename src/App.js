4cce3e7f834f116dbe74c1577993b4bb75d23118
Galiwango Ananiya <galiwango101@gmail.com>
2021-07-13 22:46:59 +0300

    Configure local linters.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  state = {  };
-  render() { 
-    return ( 
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
       <h1>Hello React!</h1>
-     );
-  };
+    );
+  }
 }
- 
+
 export default App;
