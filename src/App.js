7ba8ee8025ad7c6db4a65067627a01a1e3a567dd
Galiwango Ananiya <galiwango101@gmail.com>
2021-07-13 23:13:10 +0300

    Add style sheet.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
@@ -8,7 +9,7 @@ class App extends Component {
 
   render() {
     return (
-      <h1>Hello React!</h1>
+      <h1>Hello React.js!</h1>
     );
   }
 }
