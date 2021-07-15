fd6f7a8769862aeee452fe5909b4337a8a860d82
Galiwango Ananiya <galiwango101@gmail.com>
2021-07-16 00:22:36 +0300

    Add functionality to handle calculator number pad in Calculate module.

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -20,8 +20,27 @@ const Calculate = (() => {
     return data
   }
 
+  const handleNumberPad = (data, btnName) => {
+    let { total, next, operation } = data;
+    if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(btnName)) {
+      if (!operation && !total) {
+        total = btnName;
+      } else {
+        total += btnName;
+      }
+      if (!next) {
+        next = btnName;
+      } else {
+        next += btnName;
+      }
+    }
+
+    return data;
+  }
+
   const calculate = (data, btnName) => {
     handleCalculations(data, btnName);
+    handleNumberPad(data, btnName);
   }
 
   return { calculate };
