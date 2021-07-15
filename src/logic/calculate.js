186171ab365b5e25392d3d967efc74771d51729d
Galiwango Ananiya <galiwango101@gmail.com>
2021-07-15 20:46:13 +0300

    Implement Calculate module.

---
+++ src/logic/calculate.js
@@ -0,0 +1,26 @@
+const Calculate = (() => {
+  calculate = (data, btnName) => {
+    let { total, next, operation } = data;
+    if(btnName === '+') {
+
+    }
+    else if (btnName === '-') {
+      
+    }
+    else if (btnName === 'X') {
+      
+    }
+    else if (btnName === '÷') {
+      
+    }
+    else if (btnName === '%') {
+      
+    }
+
+    return { total, next, operation };
+  }
+
+  return { calculate };
+})();
+
+export default Calculate;
