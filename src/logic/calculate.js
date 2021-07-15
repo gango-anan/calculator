1906c0626027a5778405430895a6ad9a064c41e1
Galiwango Ananiya <galiwango101@gmail.com>
2021-07-15 22:30:49 +0300

    Implement addition operation in Operator module.

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -5,16 +5,16 @@ const Calculate = (() => {
 
     }
     else if (btnName === '-') {
-      
+
     }
     else if (btnName === 'X') {
-      
+
     }
     else if (btnName === '÷') {
-      
+
     }
     else if (btnName === '%') {
-      
+
     }
 
     return { total, next, operation };
---
+++ src/logic/operate.js
@@ -0,0 +1,19 @@
+import Big from 'big.js';
+
+const Operate = (() => {
+  const operate = (numberOne, numberTwo, operation) => {
+    const numOne = new Big(numberOne);
+    const numTwo = new Big(numberTwo);
+    let result = 0;
+
+    if (operation === '+') {
+      result = numOne.plus(numTwo);
+      return result.c[0];
+    }
+    return result;
+  };
+
+  return { operate };
+})();
+
+export default Operate;
