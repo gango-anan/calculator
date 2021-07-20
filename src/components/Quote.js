041432dc513ea45eb86e15ad2023e36131ea6f74
Galiwango Ananiya <galiwango101@gmail.com>
2021-07-20 20:51:28 +0300

    Add tests for the Home component.

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Quote = () => (
-  <div>
+  <div data-testid="quote-container">
     <h1>Quote</h1>
-    <p>
+    <p data-testid="quote">
       Somehow it’s okay for people to chuckle about not being good at math. Yet,
       if I said “I never learned to read,” they’d say I was an illiterate dolt.
     </p>
-    <p>
+    <p data-testid="quote-author">
       <em>— Neil deGrasse Tyson, American astrophysicist and author —</em>
     </p>
   </div>
