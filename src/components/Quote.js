a88be7b577b114672917700459d9db97c0aa3c44
Galiwango Ananiya <galiwango101@gmail.com>
2021-07-20 14:20:39 +0300

    Add Quote component.

---
+++ src/components/Quote.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Quote = () => (
+  <div>
+    <h1>Quote</h1>
+    <p>
+      Somehow it’s okay for people to chuckle about not being good at math. Yet,
+      if I said “I never learned to read,” they’d say I was an illiterate dolt.
+    </p>
+    <p>
+      <em>— Neil deGrasse Tyson, American astrophysicist and author —</em>
+    </p>
+  </div>
+);
+
+export default Quote;
