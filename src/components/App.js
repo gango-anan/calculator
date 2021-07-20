acd71dc8bd20c4edfd2cf743e8a315441b0cb2ce
Galiwango Ananiya <galiwango101@gmail.com>
2021-07-20 14:25:05 +0300

    Add routes to render Home, Calculator, Quote, and Navbar components.

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import DisplayPanel from './DisplayPanel';
-import ButtonPanel from './ButtonPanel';
-import Calculate from '../logic/calculate';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import Calculator from './Calculator';
+import Quote from './Quote';
 import '../App.css';
 
-const App = () => {
-  const [data, setData] = useState({
-    total: null,
-    next: null,
-    operation: null,
-  });
-
-  const handleClick = (btnName) => {
-    const calculations = Calculate.calculate(data, btnName);
-    setData({
-      total: calculations.total,
-      next: calculations.next,
-      operation: calculations.operation,
-    });
-  };
-
-  const valueToDisplay = (outPut) => {
-    const { next, operation, total } = outPut;
-    let displayedOutput = '0';
-    if (!next && !operation && total) {
-      displayedOutput = total;
-    }
-    if (!total && next && operation) {
-      displayedOutput = next;
-    }
-    if (next && total && operation) {
-      displayedOutput = total;
-    }
-    return displayedOutput;
-  };
-
-  useEffect(() => {
-    if (data.operation) {
-      document.title = `You just did a ${data.operation} operation`;
-    }
-  });
-
-  return (
+const App = () => (
+  <BrowserRouter>
     <>
-      <DisplayPanel result={valueToDisplay(data)} />
-      <ButtonPanel clickHandler={handleClick} />
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route path="/calculator" component={Calculator} />
+      <Route path="/quote" component={Quote} />
     </>
-  );
-};
+  </BrowserRouter>
+);
 
 export default App;
