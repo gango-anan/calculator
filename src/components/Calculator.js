02ef4832eea72a089be1c7a076b8b95bd7a7623b
Galiwango Ananiya <galiwango101@gmail.com>
2021-07-20 14:19:07 +0300

    Add Calculator component.

---
+++ src/components/Calculator.js
@@ -0,0 +1,66 @@
+// import React from 'react';
+
+// const Calculator = () => (
+//   <div>
+//     <h1>Calculator</h1>
+//     <p>
+//       Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+//     </p>
+//   </div>
+// );
+
+// export default Calculator;
+
+import React, { useEffect, useState } from 'react';
+import DisplayPanel from './DisplayPanel';
+import ButtonPanel from './ButtonPanel';
+import Calculate from '../logic/calculate';
+import '../App.css';
+
+const Calculator = () => {
+  const [data, setData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (btnName) => {
+    const calculations = Calculate.calculate(data, btnName);
+    setData({
+      total: calculations.total,
+      next: calculations.next,
+      operation: calculations.operation,
+    });
+  };
+
+  const valueToDisplay = (outPut) => {
+    const { next, operation, total } = outPut;
+    let displayedOutput = '0';
+    if (!next && !operation && total) {
+      displayedOutput = total;
+    }
+    if (!total && next && operation) {
+      displayedOutput = next;
+    }
+    if (next && total && operation) {
+      displayedOutput = total;
+    }
+    return displayedOutput;
+  };
+
+  useEffect(() => {
+    if (data.operation) {
+      document.title = `You just did a ${data.operation} operation`;
+    }
+  });
+
+  return (
+    <>
+      <h1>Calculator</h1>
+      <DisplayPanel result={valueToDisplay(data)} />
+      <ButtonPanel clickHandler={handleClick} />
+    </>
+  );
+};
+
+export default Calculator;
